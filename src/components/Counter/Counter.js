0f6f738784ee822567923627cfbd0104982aff15
Roman Ratushnyi <roman.ratushnyi@freshcodeit.com>
2024-07-02 18:30:38 +0300

    Add step changing

--- src/components/Counter/Counter.js
+++ src/components/Counter/Counter.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { increment, decrement } from '../../store/slices/counterSlice';
+import { increment, decrement, setStep } from '../../store/slices/counterSlice';
 
 const Counter = (props) => {
   const { count, step, dispatch } = props;
@@ -9,18 +9,26 @@ const Counter = (props) => {
   return (
     <div>
       <p>Count: {count}</p>
+      <label>
+        Set step:
+        <input
+          type="number"
+          value={step}
+          onChange={({ target: { value } }) => dispatch(setStep(value))}
+        />
+      </label>
       <p>Step: {step}</p>
       <button onClick={() => dispatch(increment())}>Increment</button>
       <button onClick={() => dispatch(decrement())}>Decrement</button>
     </div>
   );
-}
+};
 
 function mapStateToProps(state) {
   return {
     count: state.count,
-    step: state.step
+    step: state.step,
   };
 }
 
-export default connect(mapStateToProps)(Counter);;
+export default connect(mapStateToProps)(Counter);
--- src/store/slices/counterSlice.js
+++ src/store/slices/counterSlice.js
@@ -14,6 +14,9 @@ const counterSlice = createSlice({
     },
     decrement: (state, action) => {
       state.count -= state.step;
+    },
+    setStep: (state, action) => { // action = { type, payload };
+      state.step = Number(action.payload);
     }
   }
 });
@@ -21,7 +24,7 @@ const counterSlice = createSlice({
 const { reducer, actions }  = counterSlice;
 
 // action creators
-const { increment, decrement } = actions;
+const { increment, decrement, setStep } = actions;
 
-export { increment, decrement };
+export { increment, decrement, setStep };
 export default reducer;
