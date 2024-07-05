548ce9c3a7273ae18c62382e3a144e0797df1c33
Roman Ratushnyi <roman.ratushnyi@freshcodeit.com>
2024-07-05 18:31:04 +0300

    Rewrite API request to axios

--- src/api/index.js
+++ src/api/index.js
@@ -1,6 +1,7 @@
+import axios from "axios";
+
 export async function getUsers() {
-  const response = await fetch('https://jsonplaceholder.typicode.com/users');
-  const users = await response.json();
+  const { data: users } = await axios.get('https://jsonplaceholder.typicode.com/users');
 
   return users;
 }
