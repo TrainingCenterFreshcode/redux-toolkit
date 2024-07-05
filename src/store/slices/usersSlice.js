4746c5ceee457c5e5f0841084ed7f864bbc03b84
Roman Ratushnyi <roman.ratushnyi@freshcodeit.com>
2024-07-05 18:26:48 +0300

    extraReducers (builder callback notation)

--- src/store/slices/usersSlice.js
+++ src/store/slices/usersSlice.js
@@ -19,18 +19,14 @@ const initialState = {
 const usersSlice = createSlice({
   name: SLICE_NAME,
   initialState,
-  extraReducers: {
-    [getUsers.pending]: (state, action) => {
+  extraReducers: (builder) => {
+    builder.addCase(getUsers.pending, (state, action) => {
       state.isLoading = true;
-    },
-    [getUsers.fulfilled]: (state, action) => {
+    });
+    builder.addCase(getUsers.fulfilled, (state, action) => {
+      state.isLoading = false;
       state.users = action.payload;
-      state.isLoading = false;
-    },
-    [getUsers.rejected]: (state, action) => {
-      state.error = action.payload;
-      state.isLoading = false;
-    }
+    });
   }
 });
 
